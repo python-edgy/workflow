b27edaa7a80bf7cd40d5a26df114058f3795dacd
Romain Dorgueil <romain@dorgueil.net>
2016-07-22 11:26:47 +0200

    update deps

--- setup.py
+++ setup.py
@@ -10,32 +10,19 @@ def read(filename, flt=None):
         content = f.read().strip()
         return flt(content) if callable(flt) else content
 
-def requirements_filter(c):
-    install_requires = []
-    for requirement in tolines(c):
-        _pos = requirement.find('#egg=')
-        if _pos != -1:
-            requirement = requirement[_pos+5:].strip()
-        _pos = requirement.find('#')
-        if _pos != -1:
-            requirement = requirement[0:_pos].strip()
-        if len(requirement):
-            install_requires.append(requirement)
-    return install_requires
-
 version = read('version.txt')
 
 setup(
     name = 'edgy.workflow',
     description = 'Simple workflow / finite state machine management.',
     license = 'Apache License, Version 2.0',
+    install_requires = [],
     namespace_packages = ['edgy'],
     version = version,
     long_description = read('README.rst'),
     classifiers = read('classifiers.txt', tolines),
     packages = find_packages(exclude=['ez_setup', 'example', 'test']),
     include_package_data = True,
-    install_requires = read('requirements.txt', requirements_filter),
     extras_require = {'dev': ['coverage >=4.0,<4.2',
          'nose >=1.3,<1.4',
          'pylint >=1.4,<1.5',
